136dcb1bcdc8bf55802a90a2bf12654c461bb96d
Benjamin Cartier <benjamin.cartier@nventive.com>
2019-04-05 11:05:50 -0400

    Added script for VS Image creation

---
+++ Azure Build Agents/Build-VSImage.ps1
@@ -0,0 +1,35 @@
+param (
+ [int][Parameter(Mandatory)]$MajorVersion
+)
+$SupportedVersions = 15,16
+
+if(!($SupportedVersions -contains $MajorVersion)) {
+    Write-Host "Version $MajorVersion of Visual Studio is currently not supported" 
+    return
+}
+
+$Dockerfile = ""
+
+if($MajorVersion -eq 15) {
+    $Dockerfile = "vs15_enterprise.dockerfile"
+} elseif ($MajorVersion -eq 16) {
+    $Dockerfile = "vs16_enterprise.dockerfile"
+}
+
+if(![String]::IsNullOrEmpty($Dockerfile)) {
+    $TemporaryImageTag = New-Guid
+    docker build -t $TemporaryImageTag -m 4G -f vs15_enterprise.dockerfile . 
+    #Fire up a new container with the newly built image
+    $TemporaryContainerId = docker run -m 2G -dt $TemporaryImageTag
+    #Retrieve the version of VS
+    $VisualStudioVersion = docker exec $TemporaryContainerId powershell -NoLogo "(Get-ChildItem Env:VisualStudio_Version).Value"
+    #Tag the image
+    docker tag $TemporaryImageTag nventive/build-agent:$VisualStudioVersion
+    #Remove the temporary tag
+    docker rmi $TemporaryImageTag
+    #Stop the container to grad the markdown file inside of the image
+    docker stop $TemporaryContainerId
+    docker cp ${TemporaryContainerId}:'C:\InstalledSoftware.md' .\InstalledSoftware.md
+    #Remove the temporary container
+    docker rm $TemporaryContainerId
+}
